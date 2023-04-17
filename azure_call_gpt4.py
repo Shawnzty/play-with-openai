631956b7eb4f32ef7a1ebdcb99bfab3525cd7dab
Tianyi Zheng <zhengty0509@gmail.com>
2023-04-17 15:21:41 +0900

    update

--- azure_call_gpt4.py
+++ azure_call_gpt4.py
@@ -5,10 +5,7 @@ openai.api_key = "sk-"
 response = openai.ChatCompletion.create(
     model="gpt-4",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
+        {"role": "user", "content": "Who are you? and what model are you using?"},
     ]
 )
 
